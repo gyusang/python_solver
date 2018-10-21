230861241d669509f4b1bcdf50ece35688f823bc
Sanggyu Lee <sanggyu523@naver.com>
2018-10-22 08:27:59 +0900

    Format text

--- cube2.py
+++ cube2.py
@@ -1,91 +1,99 @@
 from random import randint
 from copy import copy, deepcopy
 import json
-with open('perms2.txt','r') as file:
-	perms = json.load(file)
 
-def permute(src,p, reversed=False):
-	if not reversed:
-		x = p[0]
-		block = x[0]
-		delta = x[1]
-		tmp = deepcopy(src[block[-1][0]][block[-1][1]])
-		for i in [4,3,2,1]:
-			src[block[i][0]][block[i][1]][0] = src[block[i-1][0]][block[i-1][1]][0]
-			src[block[i][0]][block[i][1]][1] = (src[block[i-1][0]][block[i-1][1]][1] + delta[i]) % 3
-		src[block[0][0]][block[0][1]][0] = tmp[0]
-		src[block[0][0]][block[0][1]][1] = (tmp[1] + delta[0]) % 3
-
-		x = p[1]
-		block = x[0]
-		delta = x[1]
-		tmp = deepcopy(src[block[-1][0]][block[-1][1]])
-		for i in [4,3,2,1]:
-			src[block[i][0]][block[i][1]][0] = src[block[i-1][0]][block[i-1][1]][0]
-			src[block[i][0]][block[i][1]][1] = (src[block[i-1][0]][block[i-1][1]][1] + delta[i]) % 2
-		src[block[0][0]][block[0][1]][0] = tmp[0]
-		src[block[0][0]][block[0][1]][1] = (tmp[1] + delta[0]) % 2
-	else:
-		x = p[0]
-		block = x[0]
-		delta = x[1]
-		tmp = deepcopy(src[block[-1][0]][block[-1][1]])
-		for i in [0,1,2,3]:
-			src[block[i-1][0]][block[i-1][1]][0] = src[block[i][0]][block[i][1]][0]
-			src[block[i-1][0]][block[i-1][1]][1] = (src[block[i][0]][block[i][1]][1] - delta[i] + 3) % 3
-
-		src[block[-2][0]][block[-2][1]][0] = tmp[0]
-		src[block[-2][0]][block[-2][1]][1] = (tmp[1] - delta[-1] + 3) % 3
-
-		x = p[1]
-		block = x[0]
-		delta = x[1]
-		tmp = deepcopy(src[block[-1][0]][block[-1][1]])
-		for i in [0,1,2,3]:
-			src[block[i-1][0]][block[i-1][1]][0] = src[block[i][0]][block[i][1]][0]
-			src[block[i-1][0]][block[i-1][1]][1] = (src[block[i][0]][block[i][1]][1] - delta[i] + 2) % 2
-
-		src[block[-2][0]][block[-2][1]][0] = tmp[0]
-		src[block[-2][0]][block[-2][1]][1] = (tmp[1] - delta[-1] + 2) % 2
+with open('perms2.txt', 'r') as file:
+    perms = json.load(file)
 
 
-def turn(state,no):
-	if not isinstance(no,int):
-		for i in no:
-			if i>0:
-				permute(state,perms[i-1])
-			elif i<0:
-				permute(state,perms[-i-1],True)
-	else:
-		i=no
-		if i>0:
-			permute(state,perms[i-1])
-		elif i<0:
-			permute(state,perms[-i-1],True)
-	return state
+def permute(src, p, reverse=False):
+    if not reverse:
+        x = p[0]
+        block = x[0]
+        delta = x[1]
+        tmp = deepcopy(src[block[-1][0]][block[-1][1]])
+        for i in [4, 3, 2, 1]:
+            src[block[i][0]][block[i][1]][0] = src[block[i - 1][0]][block[i - 1][1]][0]
+            src[block[i][0]][block[i][1]][1] = (src[block[i - 1][0]][block[i - 1][1]][1] + delta[i]) % 3
+        src[block[0][0]][block[0][1]][0] = tmp[0]
+        src[block[0][0]][block[0][1]][1] = (tmp[1] + delta[0]) % 3
 
-item_num = [10,5,20,5,10]
-solved = [[[(i,j),0] for j in range(item_num[i])] for i in range(5)]
+        x = p[1]
+        block = x[0]
+        delta = x[1]
+        tmp = deepcopy(src[block[-1][0]][block[-1][1]])
+        for i in [4, 3, 2, 1]:
+            src[block[i][0]][block[i][1]][0] = src[block[i - 1][0]][block[i - 1][1]][0]
+            src[block[i][0]][block[i][1]][1] = (src[block[i - 1][0]][block[i - 1][1]][1] + delta[i]) % 2
+        src[block[0][0]][block[0][1]][0] = tmp[0]
+        src[block[0][0]][block[0][1]][1] = (tmp[1] + delta[0]) % 2
+    else:
+        x = p[0]
+        block = x[0]
+        delta = x[1]
+        tmp = deepcopy(src[block[-1][0]][block[-1][1]])
+        for i in [0, 1, 2, 3]:
+            src[block[i - 1][0]][block[i - 1][1]][0] = src[block[i][0]][block[i][1]][0]
+            src[block[i - 1][0]][block[i - 1][1]][1] = (src[block[i][0]][block[i][1]][1] - delta[i] + 3) % 3
+
+        src[block[-2][0]][block[-2][1]][0] = tmp[0]
+        src[block[-2][0]][block[-2][1]][1] = (tmp[1] - delta[-1] + 3) % 3
+
+        x = p[1]
+        block = x[0]
+        delta = x[1]
+        tmp = deepcopy(src[block[-1][0]][block[-1][1]])
+        for i in [0, 1, 2, 3]:
+            src[block[i - 1][0]][block[i - 1][1]][0] = src[block[i][0]][block[i][1]][0]
+            src[block[i - 1][0]][block[i - 1][1]][1] = (src[block[i][0]][block[i][1]][1] - delta[i] + 2) % 2
+
+        src[block[-2][0]][block[-2][1]][0] = tmp[0]
+        src[block[-2][0]][block[-2][1]][1] = (tmp[1] - delta[-1] + 2) % 2
+
+
+def turn(state, no):
+    if not isinstance(no, int):
+        for i in no:
+            if i > 0:
+                permute(state, perms[i - 1])
+            elif i < 0:
+                permute(state, perms[-i - 1], True)
+    else:
+        i = no
+        if i > 0:
+            permute(state, perms[i - 1])
+        elif i < 0:
+            permute(state, perms[-i - 1], True)
+    return state
+
+
+item_num = [10, 5, 20, 5, 10]
+solved = [[[(i, j), 0] for j in range(item_num[i])] for i in range(5)]
+
 
 def newint(b=0, c=0):
     a = None
     while True:
-        a = randint(-12,12)
-        if a==0 or a==-b or a==-c: continue
-        else: return a
+        a = randint(-12, 12)
+        if a == 0 or a == -b or a == -c:
+            continue
+        else:
+            return a
+
 
 def mix(n=50):
-	turns = [0]
-	for i in range(n):
-		turns.append(newint(turns[-1]))
-	turns.pop(0)
-	print("turns : ", turns)
-	A = deepcopy(solved)
-	return turn(A,turns)
+    turns = [0]
+    for i in range(n):
+        turns.append(newint(turns[-1]))
+    turns.pop(0)
+    print("turns : ", turns)
+    A = deepcopy(solved)
+    return turn(A, turns)
+
 
 def mix_seq(n=50):
-	turns = [0]
-	for i in range(n):
-		turns.append(newint(turns[-1]))
-	turns.pop(0)
-	return turns
+    turns = [0]
+    for i in range(n):
+        turns.append(newint(turns[-1]))
+    turns.pop(0)
+    return turns
--- solver.py
+++ solver.py
@@ -1,12 +1,13 @@
 from cube2 import *
 from random import random, randint
+from threading import Event
 
-with open('blockinfo.txt','r') as file:
-	block_info = json.load(file)
+with open('blockinfo.txt', 'r') as file:
+    block_info = json.load(file)
 block_info = [[[tuple(k) for k in j] for j in i] for i in block_info]
 
-from threading import Event
 halt = Event()
+
 
 def fit0(state):
     score = 0
@@ -16,32 +17,33 @@ def fit0(state):
                 score += 1
     return score
 
+
 def fit1(state):
     score = 0
-    for i in [0,1,2,3,4]:
+    for i in [0, 1, 2, 3, 4]:
         for j in range(item_num[i]):
-            m=2 if (i%2 or j%2) else 3
+            m = 2 if (i % 2 or j % 2) else 3
             for k in range(m):
 
                 loc = block_info[i][j][k]
                 blk = state[i][j]
-                if block_info[blk[0][0]][blk[0][1]][(k-blk[1]+m)%m][0] != loc[0]:
+                if block_info[blk[0][0]][blk[0][1]][(k - blk[1] + m) % m][0] != loc[0]:
                     score += 1
     return score
 
 
 def fit2(state):
     score = 0
-    for i in [0,1,2,3,4]:
+    for i in [0, 1, 2, 3, 4]:
         for j in range(item_num[i]):
-            if i%2 or j%2: # edge condition
-                if state[i][j][0] == (i,j): # correct position
+            if i % 2 or j % 2:  # edge condition
+                if state[i][j][0] == (i, j):  # correct position
                     if state[i][j][1]:
                         score += 4
-                else: # not correct position
+                else:  # not correct position
                     score += 3
-            else: # corner condition
-                if state[i][j][0] == (i,j): # correct position not correct rotation
+            else:  # corner condition
+                if state[i][j][0] == (i, j):  # correct position not correct rotation
                     if state[i][j][1]:
                         score += 2
                 else:
@@ -49,38 +51,65 @@ def fit2(state):
     return score
 
 
+def fit3(state):
+    score = 0
+    for i in [0, 1, 2, 3, 4]:
+        for j in range(item_num[i]):
+            if i % 2 or j % 2:  # edge condition
+                if state[i][j][0] == (i, j):  # correct position
+                    if state[i][j][1]:
+                        score += 0
+                else:  # not correct position
+                    score += 2
+            else:  # corner condition
+                if state[i][j][0] == (i, j):  # correct position not correct rotation
+                    if state[i][j][1]:
+                        score += 0
+                else:
+                    score += 1
+    return score
+
+
+fit = fit1
+
+
 def SA(state, k=1.0, T=5000, N=10000, a=0.05):
     moves = []
     logs = []
     temp = T
     f = fit(state)
+
     def evaluate(delta):
-        if delta<=0 : return True
-        elif random()<2**(-delta/(k*temp)) : return True
-        else: return False
-    while f>0 and temp>1:
-        b, minimum, maximum = 0,f,f
+        if delta <= 0:
+            return True
+        elif random() < 2 ** (-delta / (k * temp)):
+            return True
+        else:
+            return False
+
+    while f > 0 and temp > 1:
+        b, minimum, maximum = 0, f, f
         next = None
         nextf = None
         for i in range(N):
             c = newint(b)
-            next = turn(state,c)
+            next = turn(state, c)
             nextf = fit(next)
-            if evaluate(nextf-f):
+            if evaluate(nextf - f):
                 state = next
                 f = nextf
                 b = c
-                minimum = min(minimum,f)
-                maximum = max(maximum,f)
+                minimum = min(minimum, f)
+                maximum = max(maximum, f)
                 moves.append(c)
-        temp *= 1-a;
-        print("temp:%f, fit:%d"%(temp,f))
-        logs.append((temp,f,minimum,maximum))
+        temp *= 1 - a;
+        print("temp:%f, fit:%d" % (temp, f))
+        logs.append((temp, f, minimum, maximum))
     return logs, state
 
 
-def GA(state, K=170, N=100, a = 0.8, b = 0.2, e = 0.1):
-    if N<=1 :
+def GA(state, K=170, N=100, a=0.8, b=0.2, e=0.1):
+    if N <= 1:
         print("N should be bigger than 1")
         return
     genes = []
@@ -92,54 +121,57 @@ def GA(state, K=170, N=100, a = 0.8, b = 0.2, e = 0.1):
             turns.append(newint(turns[-1]))
         turns.pop(0)
         genes.append(turns)
+
     def turn_fit(no):
         tmp = deepcopy(state)
-        return fit(turn(tmp,no))
+        return fit(turn(tmp, no))
+
     def new_pair():
-        m = newint(0,N)
-        f = newint(0,N)
-        while m==f:
-            f = newint(0,N)
-        return list(genes[m]),list(genes[f])
+        m = newint(0, N)
+        f = newint(0, N)
+        while m == f:
+            f = newint(0, N)
+        return list(genes[m]), list(genes[f])
+
     genes.sort(key=turn_fit)
     generation = 0
     try:
         while turn_fit(genes[0]) > 0 and not halt.is_set():
             pool = []
-            while len(pool) <= N*(1-e): #crossover
+            while len(pool) <= N * (1 - e):  # crossover
                 pair = new_pair()
-                initial_fit = turn_fit(pair[0]),turn_fit(pair[1])
-                if random()<a:
-                    location = randint(1,N-1)
-                    pair = [ pair[0][:location] + pair[1][location:], pair[1][location:] + pair[0][:location]]
+                initial_fit = turn_fit(pair[0]), turn_fit(pair[1])
+                if random() < a:
+                    location = randint(1, N - 1)
+                    pair = [pair[0][:location] + pair[1][location:], pair[1][location:] + pair[0][:location]]
                 for i in range(2):
                     if turn_fit(pair[i]) <= initial_fit[i]:
                         pool.append(pair[i])
-            for x in pool: # mutation
-                if random()<b:
-                    c = randint(0,K-1)
-                    if c==0:
-                        x[c] = newint(x[c+1])
-                    elif c == K-1:
-                        x[c] = newint(x[c-1])
+            for x in pool:  # mutation
+                if random() < b:
+                    c = randint(0, K - 1)
+                    if c == 0:
+                        x[c] = newint(x[c + 1])
+                    elif c == K - 1:
+                        x[c] = newint(x[c - 1])
                     else:
-                        x[c] = newint(x[c-1],x[c+1])
-            while len(pool) < N: # elitism
+                        x[c] = newint(x[c - 1], x[c + 1])
+            while len(pool) < N:  # elitism
                 pool.append(genes.pop(0))
             genes = pool
             genes.sort(key=turn_fit)
             generation += 1
             min_fits.append(turn_fit(genes[0]))
             max_fits.append(turn_fit(genes[-1]))
-            print("At generation %d, among %d genes, fit =( %d, %d)"%(generation,N,min_fits[-1],max_fits[-1]))
+            print("At generation %d, among %d genes, fit =( %d, %d)" % (generation, N, min_fits[-1], max_fits[-1]))
     except:
         print("Exception Ocurred")
     finally:
-        return turn_fit(genes[0]),genes[0],min_fits,max_fits
+        return turn_fit(genes[0]), genes[0], min_fits, max_fits
 
 
-if __name__=='__main__':
-    #NOTE SA algorithm
+if __name__ == '__main__':
+    # NOTE SA algorithm
 
     # rtn = SA(mix(),k=0.07, T=30, N=10000, a=0.005)
     # import pylab as pl
@@ -153,7 +185,7 @@ if __name__=='__main__':
     # pl.ylabel('Fitness')
     # pl.show()
 
-    #NOTE 섞는 횟수에 따른 fitness 변화
+    # NOTE 섞는 횟수에 따른 fitness 변화
 
     # import pylab as pl
     # def f(n):
@@ -174,26 +206,30 @@ if __name__=='__main__':
     # print("max : %d, avg : %d"%(max(Y[:,2]),pl.average(Y[-40:][:,0])))
     # pl.show()
 
-    #NOTE 섞는 과정에서 fitness 변화
+    # NOTE 섞는 과정에서 fitness 변화
 
     import pylab as pl
+
+
     def f(n):
         global r
         A = deepcopy(solved)
-        return fit(turn(A,r[:n]))
+        return fit(turn(A, r[:n]))
+
+
     r = mix_seq(40)
-    fit = fit1
+    fit = fit2
     for i in range(2):
         X = range(41)
         Y = pl.array([f(x) for x in X])
-        pl.plot(X,Y,'-',color='C'+str(i))
-        fit = fit2
-    pl.legend(['fit1','fit2'])
+        pl.plot(X, Y, '-', color='C' + str(i))
+        fit = fit3
+    pl.legend(['fit2', 'fit3'])
     pl.title('섞는 과정에서 fitness 변화')
     pl.xlabel('움직임 횟수')
     pl.ylabel('Fitness')
     pl.show()
 
-    #NOTE GA Algorithm
-    
+    # NOTE GA Algorithm
+
     # print(GA(mix(),K=110,N=355))
