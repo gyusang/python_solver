f599642e58858046d4252bd0b8179f65b49d5991
Sanggyu Lee <sanggyu523@naver.com>
2018-11-05 22:50:15 +0900

    Add Position Metric

--- cube2.py
+++ cube2.py
@@ -5,6 +5,7 @@ import json
 with open('perms2.txt', 'r') as file:
     perms = json.load(file)
 
+perms = [[[[tuple(k) for k in j[0]], j[1]]for j in i] for i in perms]
 
 def permute(src, p, reverse=False):
     if not reverse:
@@ -51,6 +52,17 @@ def permute(src, p, reverse=False):
         src[block[-2][0]][block[-2][1]][1] = (tmp[1] - delta[-1] + 2) % 2
 
 
+def permute_block(block, perm, reversed=False):
+    block = tuple(block)
+    for p in perm:
+        if p[0].count(block) >= 1:
+            i = p[0].index(block)
+            if not reversed:
+                return p[0][(i+1)%5]
+            else:
+                return p[0][i-1]
+
+
 def turn(state, no):
     if not isinstance(no, int):
         for i in no:
@@ -72,7 +84,6 @@ solved = [[[(i, j), 0] for j in range(item_num[i])] for i in range(5)]
 
 
 def newint(b=0, c=0):
-    a = None
     while True:
         a = randint(-12, 12)
         if a == 0 or a == -b or a == -c:
---
+++ metric.py
@@ -0,0 +1,58 @@
+from cube2 import *
+import numpy as np
+Edges = []
+Corners = []
+for i in [0, 1, 2, 3, 4]:
+    for j in range(item_num[i]):
+        if i % 2 or j % 2:
+            Edges.append((i,j))
+        else:
+            Corners.append((i,j))
+dist_edge = np.array([10]*(30*30), dtype='i8').reshape((30,30))
+dist_corner = np.array([10]*(20*20), dtype='i8').reshape((20,20))
+
+for i in range(30):
+    cur_step = [Edges[i]]
+    next_step = []
+    for rotations in range(10):
+        for block in cur_step:
+            k = Edges.index(block)
+            if dist_edge[i][k] > rotations or dist_edge[k][i] > rotations:
+                dist_edge[i][k] = dist_edge[k][i] = rotations
+            for j in range(12):
+                next = permute_block(block, perms[j])
+                if next != None:
+                    next_step.append(next)
+                next = permute_block(block, perms[j], True)
+                if next != None:
+                    next_step.append(next)
+        next_step = list(set(next_step))
+        cur_step = list(next_step)
+dist_edge = np.matrix(dist_edge)
+print(dist_edge)
+
+for i in range(20):
+    cur_step = [Corners[i]]
+    next_step = []
+    for rotations in range(10):
+        for block in cur_step:
+            k = Corners.index(block)
+            if dist_corner[i][k] > rotations or dist_corner[k][i] > rotations:
+                dist_corner[i][k] = dist_corner[k][i] = rotations
+            for j in range(12):
+                next = permute_block(block, perms[j])
+                if next != None:
+                    next_step.append(next)
+                next = permute_block(block, perms[j], True)
+                if next != None:
+                    next_step.append(next)
+        next_step = list(set(next_step))
+        cur_step = list(next_step)
+dist_corner = np.matrix(dist_corner)
+print(dist_corner)
+
+
+np.save('dist_e.npy',dist_edge)
+np.save('dist_c.npy',dist_corner)
+np.save('Edges.npy', Edges)
+np.save('Corners.npy',Corners)
